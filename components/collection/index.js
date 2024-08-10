a4043c93833536222ca82f2bdc35b368c9007489
djblanc360 <djblanc360@gmail.com>
2024-08-09 20:01:32 -0700

    fixed package.json so pulling templates and settings_data when an admin makes updates in shopify editor will not conflict with npm run dev

--- components/collection/index.js
+++ components/collection/index.js
@@ -1,3 +1,54 @@
+import Swiper from 'swiper'
+import 'swiper/css/bundle'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper/modules'
+Swiper.use([Navigation, Pagination]);
+
+
+class Carousel extends HTMLElement {
+  constructor() {
+    super();
+    this.products = JSON.parse(this.dataset.products || '[]');
+    this.settings = JSON.parse(this.dataset.settings || '{}');
+    this.initSwiper();
+    console.log('in Carousel', this.products);
+    console.log('in Carousel', this.settings);
+  }
+
+  connectedCallback() {
+    this.initSwiper();
+  }
+
+  initSwiper() {
+
+    new Swiper('.carousel', {
+      spaceBetween: 10,
+      slidesPerView: 1,
+      centeredSlides: true,
+      rewind: true,
+      breakpoints: {
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 50
+        },
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+}
+
 class Collection {
   constructor() {
     console.log('collection script loaded');
@@ -6,6 +57,7 @@ class Collection {
 
   init() {
     // init collection-specific logic
+    customElements.define('base-carousel', Carousel);
   }
 }
 
--- frontend/entrypoints/main.js
+++ frontend/entrypoints/main.js
@@ -4,3 +4,5 @@ import '../../assets/header.js';
 import '../../assets/collection.js';
 import '../../assets/grid.js';
 import '../../assets/product.js';
+import '../../assets/modal.js';
+import '../../assets/promotions.js';
